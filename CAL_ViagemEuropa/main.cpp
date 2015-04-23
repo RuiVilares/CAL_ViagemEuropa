f9e64b1c006409f3192875f543dd6bf7e529313a
Diogo Trindade <diogoguarda@netcabo.pt>
2015-04-24 00:49:29 +0100

    Deleting unnecessary code

--- CAL_ViagemEuropa/main.cpp
+++ CAL_ViagemEuropa/main.cpp
@@ -1,30 +1,6 @@
-//#include "requests.h"
 #include "application.h"
-#include "graphviewer.h"
-
 
 int main(){
 	Application app;
 	return 0;
 }
-
-/*int main()
-{
-	string src, dst;
-	double latSrc, latDst, lonSrc, lonDst, timeTravel;
-	cout << "Choose your source city: ";
-	cin >> src;
-	cout << "Choose your destination city: ";
-	cin >> dst;
-	getLatLong(src, latSrc, lonSrc);
-	getLatLong(dst, latDst, lonDst);
-
-	cout << "\n" << src << ": " << latSrc << ", " << lonSrc << endl;
-	cout << dst << ": " << latDst << ", " << lonDst << "\n";
-
-	timeTravel = getTravelDuration(latSrc, lonSrc, latDst, lonDst);
-	cout << endl << "Travel time between cities: " << timeTravel << "\n\n";
-
-	system("PAUSE");
-	return 0; 
-}*/
